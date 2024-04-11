80fc73ae21d0a1ea555ec7421e9bd6ebf56794a1
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2024-04-11 14:18:05 -0300

    Alterado: Programa05, arquivo Concatenacao.py. Adicionado mais uma forma de concatenação.

--- Logica_de_programacao/Programa05/Concatenacao.py
+++ Logica_de_programacao/Programa05/Concatenacao.py
@@ -14,10 +14,14 @@ profissao = 'Programador'
 # NOTE: essa forma não imrpime números int ou float. É necessário converter para string usando a função 'str'.
 print('Olá, meu nome é ' + nome + ', tenho ' + str(idade) + ' anos, e sou ' + profissao + '.')
 
-# modo 2: estilo C/Portugol
+# modo 2: estilo C/C++/Portugol
 # NOTE: nessa forma, os espaços são colocados automaticamente
 print('Olá, meu nome é', nome, ', tenho', idade, 'anos, e sou', profissao, '.')
 
 # modo 3: f-string
 # NOTE: essa é a melhor forma de exibir os dados, além de permitir quebra de linha com Enter, sem a necessidade de '\n'
 print(f'Olá, meu nome é {nome}, tenho {idade} anos, e sou {profissao}.')
+
+# modo 4: String format
+# NOTE: nessa forma, os valores vão ao final do comando, através da função .format()
+print('Olá, meu nome é {}, tenho {} anos, e sou {}.'.format(nome, idade, profissao))
